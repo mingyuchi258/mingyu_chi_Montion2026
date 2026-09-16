2ab69af77fd4855488c9eb3d352e0cd6b70fe401
mingyuchi258 <chimi@sheridancollege.ca>
2026-09-16 17:36:30 -0400

    task1

--- mingyu_chi_Motion2026/Assets/Scripts/Controllers/Player.cs
+++ mingyu_chi_Motion2026/Assets/Scripts/Controllers/Player.cs
@@ -10,8 +10,50 @@ public class Player : MonoBehaviour
     public GameObject bombPrefab;
     public Transform bombsTransform;
 
+    public Vector3 AbombOffset = new Vector3(0, 1, 0);
+
+    public Vector3 Bbmboffset = new Vector3(0, -1, 0);
+    public float bs = 2;
+    public int n = 3;
+
+
+
     void Update()
+    {
+        if (Keyboard.current.bKey.wasPressedThisFrame)
+        {
+            Spawnbombatoffset(AbombOffset);
+        }
+
+        if (Keyboard.current.tKey.wasPressedThisFrame)
+        {
+            spawnbomtrail(bs,n);
+        }
+    }
+
+    public void Spawnbombatoffset(Vector3 v)
+    {
+        Vector3 bombp = transform.position + v;
+        Instantiate(bombPrefab, bombp, Quaternion.identity);
+
+    }
+
+
+    public void spawnbomtrail(float bs,int n)
+    {
+        for (int i = 0; i <= n; i++)
+        {
+            Vector3 bp = Bbmboffset * bs * i;
+
+            Spawnbombatoffset(bp);
+        }
+    }
+
+
+    public void spawnbombonrandomcorner(float d)
     {
 
     }
+
+
 }
