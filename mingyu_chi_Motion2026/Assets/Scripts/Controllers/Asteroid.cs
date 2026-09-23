5487f1e58e47ca68e98628a8ffc5ea17acfcc2a6
mingyuchi258 <chimi@sheridancollege.ca>
2026-09-23 13:57:58 -0400

    task2

--- mingyu_chi_Motion2026/Assets/Scripts/Controllers/Asteroid.cs
+++ mingyu_chi_Motion2026/Assets/Scripts/Controllers/Asteroid.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Asteroid : MonoBehaviour
@@ -7,14 +9,35 @@ public class Asteroid : MonoBehaviour
     public float moveSpeed;
     public float arrivalDistance;
     public float maxFloatDistance;
+    public Vector3 newposition;
 
     // Start is called before the first frame update
     void Start()
     {
+        chooseposition();
     }
 
     // Update is called once per frame
     void Update()
     {
+        asteroidmovement();
+    }
+
+
+    public void chooseposition()
+    {
+        Vector2 nd = Random.insideUnitCircle * maxFloatDistance;
+
+        Vector3 direction = new Vector3(nd.x, nd.y, 0f);
+        newposition = transform.position + direction;
+    }
+
+    public void asteroidmovement()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, newposition, moveSpeed * Time.deltaTime);
+        if (Vector3.Distance(transform.position, newposition) <= arrivalDistance)
+        {
+            chooseposition();
+        }
     }
 }
