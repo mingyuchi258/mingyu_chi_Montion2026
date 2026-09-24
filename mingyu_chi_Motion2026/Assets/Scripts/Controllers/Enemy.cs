027534f7223b13df88ee0768e2af74f0610c39b9
mingyuchi258 <chimi@sheridancollege.ca>
2026-09-24 17:06:17 -0400

    task4

--- mingyu_chi_Motion2026/Assets/Scripts/Controllers/Enemy.cs
+++ mingyu_chi_Motion2026/Assets/Scripts/Controllers/Enemy.cs
@@ -1,10 +1,23 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public Transform player;
+
+    public float speed = 1;
+    public float distance = 3;
     private void Update()
     {
+        EnemyMovement();
+    }
+
+    public void EnemyMovement()
+    {
+        Vector3 direction = player.position - transform.position;
+        Vector3 newp= transform.position + direction.normalized * distance;
+        transform.position = Vector3.MoveTowards(transform.position, newp, speed * Time.deltaTime);
     }
 
 }
